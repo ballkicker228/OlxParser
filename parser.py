5b0d3fec829cf96e09501f10b300dbfe6ab41f37
ballkicker228 <akas2008ko@gmail.com>
2023-10-04 23:17:45 +0600

    Some fixes

--- parser.py
+++ parser.py
@@ -5,12 +5,13 @@ from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 
 class Announcement:
-    def __init__(self, name, price, date, address, status):
+    def __init__(self, *, name, price, date, address, status, link):
         self.name = name
         self.price = price
         self.date = date
         self.address = address
         self.status = status
+        self.link = link
 
 class Category:
     def __init__(self, name):
@@ -22,14 +23,19 @@ class Parser:
         pass 
 
     def parse(self, category: Category):
+        links = []
+        announcementslist = []
         options = Options()
         options.add_argument('--headless')
         driver = webdriver.Firefox(options=options) 
         driver.get(category.category)
         listofannouncementsdriver = driver.find_element(By.XPATH, '/html/body/div[1]/div[2]/div[2]/form/div[5]/div/div[2]')
-        announcementslist = listofannouncementsdriver.find_elements(By.CLASS_NAME, "css-1sw7q4x")
-        for announcement in announcementslist:
-            link = announcement.find_element(By.CLASS_NAME, "css-rc5s2u")
-            print(link.get_attribute("href"))
-            print("\n ---------------------\n")
-        #print(html)
+        announcementslistdiv = listofannouncementsdriver.find_elements(By.CLASS_NAME, "css-1sw7q4x")
+        for announcementdiv in announcementslistdiv:
+            
+            link = announcementdiv.find_element(By.CLASS_NAME, "css-rc5s2u").get_attribute("href")
+            
+
+            #announcementslist.append(Announcement(name=name, price=price, date=date, address=address, status=status, link=link))
+        
+        print(links)
